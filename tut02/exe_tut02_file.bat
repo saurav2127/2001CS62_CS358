dd5fcd773c32c7ea6b6857af378a915aee174415
Saurav Dudhate <75058098+saurav2127@users.noreply.github.com>
2023-03-28 23:27:00 +0530

    Create exe_tut02_file.bat

---
+++ tut02/exe_tut02_file.bat
@@ -0,0 +1,10 @@
+pip install scapy
+
+cd Desktop/
+cd SAURAV/
+
+sudo python3 ARP_request_response.py
+sudo python3 DNS_request_response.py
+sudo python3 PING_request_response.py
+sudo python3 TCP_3_way_handshake_start.py
+sudo python3 TCP_handshake_close.py
