d35498a0e2c644bda5e2372c786ad03c6f826f37
Saurav Dudhate <75058098+saurav2127@users.noreply.github.com>
2023-03-28 23:39:05 +0530

    Update exe_tut02_file.bat

--- tut02/exe_tut02_file.bat
+++ tut02/exe_tut02_file.bat
@@ -8,3 +8,5 @@ sudo python3 DNS_request_response.py
 sudo python3 PING_request_response.py
 sudo python3 TCP_3_way_handshake_start.py
 sudo python3 TCP_handshake_close.py
+sudo python3 FTP_connection_open.py
+sudo python3 FTP_connection_close.py
