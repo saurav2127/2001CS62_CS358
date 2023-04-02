72e0c6ae9c29feecdc5c27a5d612a85562050319
Saurav Dudhate <75058098+saurav2127@users.noreply.github.com>
2023-04-03 04:04:47 +0530

    Create exe_tut03_file.bat

---
+++ tut03/exe_tut03_file.bat
@@ -0,0 +1,21 @@
+#Move to the directory where this files exist
+cd Desktop
+
+#Run any one server in this manner
+python3 server1.py {IP address} {port}
+python3 server2.py {IP address} {port}
+python3 server3.py {IP address} {port}
+python3 server4.py {IP address} {port}
+
+#Run the client file
+python3 client.py {IP address} {port}
+
+#Enter the expression
+4+5
+
+#Output is displayed
+9.0
+
+#Enter your choice whether you have to continue or not
+Y/N
+
